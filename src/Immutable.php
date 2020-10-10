aabd4a76791e34545f80c55281c98504909fbe20
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-10-10 11:07:01 -0600

    fix nightmare

--- src/Immutable.php
+++ src/Immutable.php
@@ -4,31 +4,13 @@ namespace Defenestrator\Traits;
 
 use Cake\Chronos\Chronos;
 use Illuminate\Foundation;
-use Illuminate\Support\Hash;
 
 trait Immutable
 {   
-    protected static $checkHash = true;
+    public static $hashCheck = true;
     
     /**
-     * :::: Laravel Eloquent Immutable Trait Documentation ::::
-     * 
-     * This package is not optimal, but it may be sufficient.
-     * 
-     * The hashedContent() method returns 'state' by default,
-     * The hashColumn() method returns 'hash' by default, 
-     *  
-     * Use of created_at is acceptable but not required.
-     * Use of updated_at implies mutability, so using
-     * Blueprint::timestamps() is discouraged.
-     * 
-     * Hash checking may be overriden simply by 
-     * setting static::$checkHash = false 
-     * in the model's boot() method,
-     * this may not be a good idea.
-     *      
-     * Typing is not the bottleneck.
-     * Endeavor to be thoughtgful.
+     * :::: Laravel Eloquent Immutable Trait ::::
      * 
      * @return void
     */
@@ -43,27 +25,27 @@ trait Immutable
         });
 
         static::creating(function ($model) {
-            if (static::$checkHash) {
-                $model->{$model->hashKey()} = Hash::make($model->{$model->contentKey()});
+            if (static::$hashCheck) {
+                $model->{$model->hashColumn()} = sha1($model->{$model->hashedContent()});
             }            
             $model->created_at = $model->freshTimestamp();
         });
         
         static::retrieved(function ($model) {
-            if (static::$checkHash) {
-                if ( Hash::check($model->contentKey()) ) {
+            if (static::$hashCheck) {
+                if (sha1(strval($model->{$model->hashedContent()})) != strval($model->{$model->hashColumn()})) {
                     abort(500, 'Hash check failed. Immutable model data is corrupt.');
                 }
             }   
         });
     }
     
-    protected function hashedColumn() : string
+    public function hashColumn() : string
     {
         return 'hash';
     }
 
-    protected function hashedContent() : string
+    public function hashedContent() : string
     {
         return 'state';
     }
